cd7b61ad408420be8bae9d07b0e820602738c8f8
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2025-04-26 19:27:19 +0700

    .

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3e99aa010ce1f9dfcdbe6f70e51c8cc5",
+const RESOURCES = {"flutter_bootstrap.js": "6adfa5a5bd8bf9dbf75244eaac11450b",
 "version.json": "7f86edf6c13624ed15391b7ecd82e7d0",
 "index.html": "5216f2958ee3c3848a75867a246f20ba",
 "/": "5216f2958ee3c3848a75867a246f20ba",
-"main.dart.js": "3eb361ee5c7c8b78240c0d4516825050",
+"main.dart.js": "7833b77e280930d323317693ff37f4bf",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
